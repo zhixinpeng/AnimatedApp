f0040d74ec05d41782ce4d3e6e2b5b97beaab270
彭智鑫 <pengzhixin@live.cn>
2022-10-13 16:47:03 +0800

    Initial Commit

---
+++ AnimatedApp/AnimatedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedAppApp.swift
+//  AnimatedApp
+//
+//  Created by 彭智鑫 on 2022/10/13.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AnimatedApp
+//
+//  Created by 彭智鑫 on 2022/10/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
